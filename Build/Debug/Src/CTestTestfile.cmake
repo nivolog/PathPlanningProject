84cd752734a610d926a9b873b180b9b51b9564a7
nivolog <golovin.va@phystech.edu>
2020-08-11 11:55:23 +0300

    Removed from git repository

--- Build/Debug/Src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vlad/PathPlanningProject/Src
-# Build directory: /home/vlad/PathPlanningProject/Build/Debug/Src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
